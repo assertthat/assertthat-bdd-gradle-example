7eba0e9cf04d900465a91a2e606e753e783f7b48
Glib Briia <glib.briia@gmail.com>
2018-12-08 23:19:36 +0000

    Initial commit

---
+++ src/test/java/Stepdefs.java
@@ -0,0 +1,27 @@
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import static org.junit.Assert.assertTrue;
+
+public class Stepdefs {
+
+    @Given("I add some meaningful step definishions instead of this dummy one")
+    public void i_add_some_meaningful_step_definishions_instead_of_this_dummy_one() {
+        boolean stepDefsCreated = true;
+        assertTrue(stepDefsCreated);
+    }
+
+    @When("I run the framework with mvn integration-test goal")
+    public void i_run_the_framework_with_mvn_integration_test_goal() {
+        boolean testsRun = true;
+        assertTrue(testsRun);
+    }
+
+    @Then("I see cucumber test report on AssertThat reporting dashboard")
+    public void i_see_cucumber_test_report_on_AssertThat_reporting_dashboard() {
+        boolean reportSubmitted = true;
+        assertTrue(reportSubmitted);
+    }
+
+}
